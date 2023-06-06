dcec534e12b4061ecadaa4d0bc519ab27affb6a5
Local2Debian <kuznecovmisa428@gmail.com>
2023-06-07 00:44:23 +0300

    feat(components): Added refresh button and input search
    Search input must set dynamic search query param
    Refresh button must unset filters for view data

---
+++ components/SearchInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="search" class="flex items-center h-10 py-2.5 pr-2.5 pl-0.5 rounded shadow shadow-black-[0.05] bg-white">
+    <input class="w-full border-none outline-none p-0.5 mr-2.5"
+      type="text"
+      v-model="searchStr" />
+    <nuxt-link :to="{
+      path: $route.path,
+      query: { search: searchStr }
+    }">
+      <SearchIcon />
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+import SearchIcon from './icons/SearchIcon.vue';
+
+export default {
+  data() {
+    return {
+      searchStr: ""
+    };
+  },
+  components: { SearchIcon }
+}
+</script>
