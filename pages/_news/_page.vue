8b672ff869fd8db6c67f0e36c13eac62233cb5ab
Local2Debian <kuznecovmisa428@gmail.com>
2023-06-07 09:56:34 +0300

    fix(pages) Added markup for news page

--- pages/_news/_page.vue
+++ pages/_news/_page.vue
@@ -1,11 +1,51 @@
 <template>
-  <div>
-    <h1>Hello World!</h1>
+  <div id="container-rss" class="flex flex-col">
+    <div id="filter" class="flex fustify-between my-6">
+      <div id="nav" class="flex gap-x-4">
+        <nuxt-link :class="['text-sm', 'font-bold', {'text-[#0029FF]': $nuxt.$route.params.news != 'all'}]" to="/all/1">Все</nuxt-link>
+        <nuxt-link :class="['text-sm', 'font-bold', {'text-[#0029FF]': $nuxt.$route.params.news != 'mos'}]" to="/mos/1">Mos.ru</nuxt-link>
+        <nuxt-link :class="['text-sm', 'font-bold', {'text-[#0029FF]': $nuxt.$route.params.news != 'lenta'}]" to="/lenta/1">Lenta.ru</nuxt-link>
+      </div>
+      <div id="type-view" class="flex flex-row grow justify-end gap-x-2.5">
+        <div id="list" @click="() => viewTypeToggle = true">
+          <ListIcon :class="['fill-[#C4C4C4]', {'fill-blue': viewTypeToggle}]"/>
+        </div>
+        <div id="table" @click="() => viewTypeToggle = false">
+          <TableIcon :class="['fill-[#C4C4C4]', {'fill-blue': !viewTypeToggle}]"/>
+        </div>
+      </div>
+    </div>
+    <div id="news">
+        <h1>{{ $route.params.news }}</h1>
+        <h1>{{ $route.params.page }}</h1>
+    </div>
+    <div id="pagination">
+
+    </div>
   </div>
 </template>
 
 <script>
+import ListIcon from '~/components/icons/ListIcon.vue';
+import TableIcon from '~/components/icons/TableIcon.vue';
+
 export default {
-  middleware: 'validate-route'
+    data() {
+        return {
+            viewTypeToggle: true,
+        };
+    },
+    middleware: "validate-route",
+    mounted() {
+        console.log(this.$route.params);
+    },
+    components: { ListIcon, TableIcon }
 }
 </script>
+
+
+<style scoped>
+.fill-blue {
+  fill: #0029FF !important;
+}
+</style>
