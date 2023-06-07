f5f3d4586820311f72b6e9096fd5596a5e828946
Local2Debian <kuznecovmisa428@gmail.com>
2023-06-07 15:10:01 +0300

    feat(components): Added data view component for rss feeds

---
+++ components/DataView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrap">
+    <div class="item">
+      <h1>{{ $props.viewType }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    viewType: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
