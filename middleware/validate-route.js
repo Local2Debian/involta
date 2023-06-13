ff717da503061f725e913560c4b66d8c2af0020d
Local2Debian <kuznecovmisa428@gmail.com>
2023-06-13 10:55:30 +0300

    fix(middleware): now route validate all routes by this path /:news/:page*

--- middleware/validate-route.js
+++ middleware/validate-route.js
@@ -1,6 +1,6 @@
 
-export default async ({ redirect, route }) => {
-  if(!/^\/(mos|lenta|all)\/\d+/.test(route.fullPath) | route.params.page <= 0){
+export default async ({ redirect, route, next }) => {
+  if(!/^\/(\d+|\w+)\/\d+/.test(route.fullPath) | route.params.page <= 0){
     await redirect('/all/1')
   }
 }
