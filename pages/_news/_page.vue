ad6ae5db0bba4bb77372228f18b6a1b8003725c4
Local2Debian <kuznecovmisa428@gmail.com>
2023-06-13 10:41:03 +0300

    fix(pages): the main logic has been moved to the layout

--- pages/_news/_page.vue
+++ pages/_news/_page.vue
@@ -1,118 +1,33 @@
 <template>
-  <div id="container-rss">
-    <div id="filter">
-      <div id="nav">
-        <nuxt-link to="/all/1">Все</nuxt-link>
-        <nuxt-link to="/mos/1">Mos.ru</nuxt-link>
-        <nuxt-link to="/lenta/1">Lenta.ru</nuxt-link>
-      </div>
-      <div id="type-view">
-        <div id="list" @click.capture="toggleView($event.currentTarget.id)">
-          <ListIcon :class="['icon', {'active': viewTypeToggle}]"/>
-        </div>
-        <div id="table" @click="toggleView($event.currentTarget.id)">
-          <TableIcon :class="['icon', {'active': !viewTypeToggle}]"/>
-        </div>
-      </div>
-    </div>
-    <div id="news">
-      <DataView :viewType="getViewType"/>
-    </div>
-    <div id="pagination">
-
+  <div class="container-rss">
+    <div class="news">
+      <GridView v-show="!viewType"/>
+      <ListView v-show="viewType"/>
     </div>
   </div>
 </template>
 
 <script>
-import DataView from '~/components/DataView.vue';
-import ListIcon from '~/components/icons/ListIcon.vue';
-import TableIcon from '~/components/icons/TableIcon.vue';
+import GridView from '~/components/GridView.vue';
+import ListView from '~/components/ListView.vue';
 import { mapState } from 'vuex'
 
 export default {
-  data() {
-    return {
-      viewTypeToggle: true,
-    };
-  },
-
-  methods: {
-    toggleView(targetId) {
-      if(targetId == 'list') this.viewTypeToggle = true;
-      if(targetId == 'table') this.viewTypeToggle = false;
-
-      window.localStorage.setItem('viewType', `${this.viewTypeToggle}`)
-    }
-  },
-
   computed: {
-    getViewType() {
-      return this.viewTypeToggle
-    },
-
-    ...mapState(['feeds'])
+    ...mapState('feedStore', ['viewType']),
   },
 
-  async asyncData({ store  }) {
-    await store.dispatch('feeds/loadFeeds')
-  },
-
-  beforeMount() {
-    const viewType = window.localStorage.getItem('viewType')
-    if(viewType){
-      this.viewTypeToggle = viewType === 'true' ? true : false
-    } else {
-      window.localStorage.setItem('viewType', `${this.viewTypeToggle}`)
-    }
-  },
-
-  components: { ListIcon, TableIcon, DataView }
-  //.fill-blue {
-//  fill: #0029FF !important;
-//}
+  components: { GridView, ListView }
 }
 </script>
 
 
 <style lang="postcss" scoped>
-#container-rss{
-  @apply flex;
-  @apply flex-col;
+.container-rss{
+  @apply flex flex-col grow;
 
-  #filter{
-    @apply flex;
-    @apply my-6;
-    @layer fustify-between;
-
-    #nav{
-      @apply flex;
-      @apply gap-x-4;
-
-      a{
-        @apply text-sm;
-        @apply font-bold;
-        @apply text-[#0029FF];
-      }
-
-      a.nuxt-link-exact-active{
-        @apply text-black;
-      }
-    }
-
-    #type-view{
-      @apply flex;
-      @layer flex-row;
-      @apply grow;
-      @apply justify-end;
-      @apply gap-x-2.5;
-      .icon{
-        @apply fill-[#C4C4C4];
-      }
-      .icon.active{
-        @apply fill-[#0029FF] !important;
-      }
-    }
+  .news{
+    @apply flex flex-col h-full;
   }
 }
 </style>
