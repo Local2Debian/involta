ebf131ec11ba7efb6451adbeb4d4bb07720dd927
Local2Debian <kuznecovmisa428@gmail.com>
2023-06-13 10:49:40 +0300

    fix(server): for api added handler for search queries

--- server/api.js
+++ server/api.js
@@ -1,9 +1,40 @@
-const express = require('express')
-const router = express.Router()
+const app = require('express')()
+const { Client } = require('redis-om')
+const feedSchema = require('./redis/schemas/FeedSchema')
 
+app.get('/search', async (req, res, error) => {
+  try{
+    const feedsCount = req.query.feedsOnPageCount
+    const currentPage = req.query.currentPage
+    const feedsOrigin = req.query.newsOrigin !== 'all' ? process.env.RSS_LINKS.trim().split(',').find(link => link.match(req.query.newsOrigin)).split('/')[2] : null
+    const searchInFeed = req.query.searchInFeed
 
-router.get('/api/test', (req, res) => {
-  res.json({ message: 'Hello from server API!' })
+    const client = await new Client().open('redis://localhost:6379')
+
+    if(client.isOpen()){
+      const feedRepository = client.fetchRepository(feedSchema)
+      const query = feedRepository.search()
+
+      if(feedsOrigin && searchInFeed)
+        query.where('origin').eq(feedsOrigin).and('title').matches(searchInFeed).or('description').matches(searchInFeed)
+
+      if(searchInFeed)
+        query.where('title').matches(searchInFeed).or('description').matches(searchInFeed)
+
+      if(feedsOrigin)
+        query.where('origin').eq(feedsOrigin)
+
+      const pageFeeds = await query.sortDescending('pubDate').return.page((currentPage - 1) * feedsCount, feedsCount)
+      const totalCount = await query.return.count()
+      res.json({
+        feeds: pageFeeds,
+        totalCount: totalCount
+      })
+    }
+    await client.close()
+  }catch(err){
+    console.log('Error:' + err);
+  }
 })
 
-module.exports = router
+module.exports = app
