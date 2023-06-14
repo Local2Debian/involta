42c174f4aa6c34c50d814ca0a23860baa532e0a6
Local2Debian <kuznecovmisa428@gmail.com>
2023-06-14 16:36:51 +0300

    fix(pages): feeds load returned on the page

--- pages/_news/_page.vue
+++ pages/_news/_page.vue
@@ -10,12 +10,21 @@
 <script>
 import GridView from '~/components/GridView.vue';
 import ListView from '~/components/ListView.vue';
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   computed: {
     ...mapState('feedStore', ['viewType']),
   },
+
+  methods: {
+    ...mapActions('feedStore', ['loadFeeds']),
+  },
+
+  async asyncData({ store, params, route }) {
+    await store.dispatch('feedStore/loadFeeds', {...params, ...route.query})
+  },
+
 
   components: { GridView, ListView }
 }
